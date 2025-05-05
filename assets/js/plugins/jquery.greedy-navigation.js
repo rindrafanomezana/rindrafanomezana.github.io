e634526ea65b435f87ee0013e175dc967fc83bcb
Rindra Ranaivomanana <71132563+rindrafanomezana@users.noreply.github.com>
2025-05-04 20:35:18 -0400

    Update jquery.greedy-navigation.js

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -15,46 +15,45 @@ var breaks = [];
 
 function updateNav() {
 
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+//  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
 
   // The visible list is overflowing the nav
   //if ($vlinks.width() > availableSpace) {
-  if (1>0) {
 
-    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
+    //while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
       // Record the width of the list
-      breaks.push($vlinks.width());
+     // breaks.push($vlinks.width());
 
       // Move item to the hidden list
       $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
 
-      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
+      //availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
 
       // Show the dropdown btn
       $btn.removeClass("hidden");
-    }
+    //}
 
     // The visible list is not overflowing
-  } else {
+ // } else {
 
     // There is space for another item in the nav
-    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
+    //while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
       // Move the item to the visible list
-      if ($vlinks_persist_tail.children().length > 0) {
-        $hlinks.children().first().insertBefore($vlinks_persist_tail);
-      } else {
-        $hlinks.children().first().appendTo($vlinks);
-      }
-      breaks.pop();
-    }
+     // if ($vlinks_persist_tail.children().length > 0) {
+      //  $hlinks.children().first().insertBefore($vlinks_persist_tail);
+     // } else {
+      //  $hlinks.children().first().appendTo($vlinks);
+     // }
+    //  breaks.pop();
+//    }
 
     // Hide the dropdown btn if hidden list is empty
-    if (breaks.length < 1) {
-      $btn.addClass('hidden');
-      $btn.removeClass('close');
-      $hlinks.addClass('hidden');
-    }
-  }
+  //  if (breaks.length < 1) {
+    //  $btn.addClass('hidden');
+   //   $btn.removeClass('close');
+   //   $hlinks.addClass('hidden');
+  //  }
+//  }
 
   // Keep counter updated
   $btn.attr("count", breaks.length);
