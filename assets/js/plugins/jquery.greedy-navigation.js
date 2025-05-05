ee4830c5d4021bb3a29cc31b485ec5d2743bde40
Rindra Ranaivomanana <71132563+rindrafanomezana@users.noreply.github.com>
2025-05-04 20:27:46 -0400

    Update jquery.greedy-navigation.js

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -15,7 +15,7 @@ var breaks = [];
 
 function updateNav() {
 
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 300;
 
   // The visible list is overflowing the nav
   if ($vlinks.width() > availableSpace) {
